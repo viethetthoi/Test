12074dbae2426f6e99f3a87503ae2b9884c8528d
viethetthoi <130229331+viethetthoi@users.noreply.github.com>
2026-05-06 21:03:20 +0700

    qssqs

--- save.php
+++ save.php
@@ -1,2 +1,30 @@
 <?php
-print_r($_POST);
+
+echo "<pre>";
+
+foreach ($_POST as $section => $tasks) {
+
+    echo strtoupper($section) . "\n";
+
+    foreach ($tasks as $taskIndex => $task) {
+
+        echo "  Task $taskIndex (Main: " . $task['main'] . ")\n";
+
+        if (!empty($task['work'])) {
+            foreach ($task['work'] as $workIndex => $work) {
+
+                echo "    Work: " . $work['name'] . "\n";
+
+                if (!empty($work['sub'])) {
+                    foreach ($work['sub'] as $sub) {
+                        echo "      - Sub: $sub\n";
+                    }
+                }
+            }
+        }
+    }
+
+    echo "\n----------------------\n";
+}
+
+echo "</pre>";
